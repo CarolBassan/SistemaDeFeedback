5920be78f0c0d31f293d5f1272541f3047cb43b6
Rafael Tischler <rafaelmullertischler@gmail.com>
2025-07-21 14:43:42 -0300

    cadastro e login funcionando (à princípio)

--- View/dashboard.php
+++ View/dashboard.php
@@ -1,12 +1,24 @@
+<?php
+include 'header.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Painel de Controle - Dashboard</h1>
     <p>Bem-vindo ao sistema de feedback de veículos.</p>
 </body>
+
 </html>
+
+<?php
+include 'footer.php';
+?>
