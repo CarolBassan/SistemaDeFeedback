97d5d46b51873f3f108622a19b6c0c23ebbca0b0
Carolini Bassan <bassan804@gmail.com>
2025-07-21 15:22:13 -0300

    tela de avalicao criada

--- View/sucesso.php
+++ View/sucesso.php
@@ -1,16 +1,25 @@
+<?php
+include 'header.php'
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <title>Cadastro realizado com sucesso</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 
 <body>
-    <div class="container">
+    <div class="login-container">
         <div class="success">&#10004;</div>
         <h2>Cadastro realizado com sucesso!</h2>
         <p>Seu usuário foi cadastrado no sistema.</p>
         <a href="login.php" class="btn">Ir para o Login</a>
     </div>
 </body>
+
+<?php
+include 'footer.php'
+?>
